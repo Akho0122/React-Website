6e2fd80e3127e73e9dada2e50a40271953e194a1
Akhror <mr.akhrororibjonov@gmail.com>
2021-12-13 17:56:26 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import {GlobalStyles} from "./GlobalStyles";
-import './App.css';
-import {lazy, Suspense} from "react";
+import { GlobalStyles } from "./GlobalStyles";
+import "./App.css";
+import { lazy, Suspense } from "react";
 
 const Home = lazy(() => import("./Pages/Home"));
 const Header = lazy(() => import("./components/Header/Header"));
 const Footer = lazy(() => import("./components/Footer/Footer"));
 const ScrollToTop = lazy(() => import("./components/ScrollToTop/ScrollToTop"));
 
-
 function App() {
   return (
     <>
       <Suspense fallback={null}>
-          <GlobalStyles/>
-          <Header/>
-          <Home/>
-          <Footer/>
-          <ScrollToTop/>
+        <GlobalStyles />
+        <Header />
+        <Home />
+        <Footer />
+        <ScrollToTop />
       </Suspense>
     </>
   );
